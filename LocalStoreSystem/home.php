9c262646db8f2820608d9b0a5721076ad88df342
Rhainier Avelino <122976115+RhainierAvelino@users.noreply.github.com>
2025-02-01 14:58:31 +0800

    Add files via upload

--- LocalStoreSystem/home.php
+++ LocalStoreSystem/home.php
@@ -160,7 +160,7 @@ include 'components/add_cart.php';
                      class="cat"><?= $fetch_products['category']; ?></a>
                   <div class="name"><?= $fetch_products['name']; ?></div>
                   <div class="flex">
-                     <div class="price"><span>$</span><?= $fetch_products['price']; ?></div>
+                     <div class="price"><span>₱ </span><?= $fetch_products['price']; ?></div>
                      <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
                   </div>
                </form>
